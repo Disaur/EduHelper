1ecd3fdc1997ce12be4fd3e095b5ac5544610c54
Disaur <disaurdai@gmail.com>
2022-06-30 23:36:25 +0800

    Initial Commit

---
+++ EduHelper/EduHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduHelperApp.swift
+//  EduHelper
+//
+//  Created by Disaur on 30/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct EduHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
